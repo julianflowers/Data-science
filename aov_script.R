9fae9c3d67f4dcae907278e6f1788bf44bc6ffb1
Julian Flowers <julian.flowers12@gmail.com>
2017-06-02 12:53:57 +0100

    Updates

--- aov_script.R
+++ aov_script.R
@@ -1,6 +1,7 @@
 ## Script for processing atlas of variation spreadsheets into tidy datasets for conversion to PHOLIO format
 ##==============================
 
+## load packages
 library(readxl)
 library(tidyverse)
 devtools::install_github(c("hadley/xml2",
@@ -8,20 +9,48 @@ devtools::install_github(c("hadley/xml2",
                            "rsheets/cellranger",
                            "rsheets/rexcel",
                            "rsheets/jailbreakr"))
-install.packages("rexcel")
+library(rexcel)
+library(jailbreakr)
 
 ## test with 2015 diagnostic atlas
-
+## download file
 url <- "https://fingertips.phe.org.uk/documents/DiagAtlasData_291116.xlsx"
 destfile <- "aov_diag.xlsx"
 aov_diag <- download.file(url, destfile)
 
+setwd("~/Documents/R_projects/timeSeries")
+
 ## List sheets
 sheets <- data.frame(excel_sheets("aov_diag.xlsx"))
 
+## Read in workbook
+test_sheet <- rexcel_read_workbook("aov_diag.xlsx")
+
+
+## Import all sheets
+test_sheet$sheets
+
+
+### Store sheet 1 as a table
+tables <- split_sheet(test_sheet$sheets$`1`)
+
+str(tables)
+
+
+### Extract values from sheet
+library(purrr)
+data_list <- map(tables, function(x) x$values())
+
 sheet <- sheets %>% as.list()
 
-## Download sheet names - function to extract map title
+sheet[1]
+
+
+# Engine
+## Download sheet names - function to extract map title - this is at the top of the each datasheet in the
+## second row and column
+
+## this function extracts the indicator names
 map_title <- function(sheet = sheet){
   title <- read_excel("aov_diag.xlsx", sheet = sheet, skip = 1)
   names(title %>% slice(2) %>% select(2))
@@ -40,25 +69,44 @@ for(i in sheet$excel_sheets..aov_diag.xlsx..){
 map_lookup <- titles %>% unlist() %>% data.frame() 
 colnames(map_lookup) <- "maps"
 map_lookup <- map_lookup %>% 
-  separate(maps, c("map", "title"), sep = ":")
+  separate(maps, c("map", "title"), sep = ":") 
 
-#### Given lack of consistency between sheets - start by downloading each on separately 
+### identify CCG indicators
+
+ccg_map <- map_lookup %>%
+  filter(stringr::str_detect(title, "CCG"))
+
+ccg_indicators <- ccg_map %>% separate(map, c("map", "number"))
+ccg_list <- ccg_indicators %>% select(number) %>% as.list()
+
+#### Given lack of consistency between sheets - in some cases data starts on 3rd row (1, 10, 11, 17, 18); in others on the 4th
+
+
+ccg_list1 <- c("1", "10", "17", "18")
+ccg_list2 <- c("2",  "3",  "4a", "5a", "6a", "9",  "15", "16", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "34", "36")
 ## Create a .csv file for each sheet.
 
 ### Read them all back in and identify which ones we have successfully parsed with column names
-
+### some files need to skip 3 rows, some to skip 2 rows; also need to remove "/" from sheet 23
 files <- list()
-for(i in sheet$excel_sheets..aov_diag.xlsx..){
+
+for(sheet in ccg_list1){
   
-  test <- read_excel("aov_diag.xlsx", sheet = i, skip = 2)
-  files <- write_csv(test, paste0(map_lookup[i, 2], ".xlsx"))
+  test <- read_excel("aov_diag.xlsx", sheet = sheet, skip = 3)
+  files <- write_csv(test, paste0(map_lookup[sheet, 2], ".csv"))
   
 }
 
+for(sheet in ccg_list2){
+  
+  test <- read_excel("aov_diag.xlsx", sheet = sheet, skip = 2) 
+  files <- write_csv(test, paste0(map_lookup[sheet, 2], ".csv"))
+  
+}
 
 f <- list.files(pattern = ".csv") ## find the .csv files
 
-## select ccg
+## select ccg files
 
 f_ccg <- f[grepl("CCG", f)]
 
@@ -66,12 +114,21 @@ df <- data.frame()
 
 for(f in f_ccg){
   
-  f1 <- read_csv(f_ccg[f]) %>% mutate(file = f_ccg[f])
+  f1 <- map(f_ccg, read_csv)
+  
+  f2 <- f_ccg[1] %>%
+    janitor::clean_names() %>%
+    select(contains("CCG|year"))
+    
+    %>% mutate(file = f_ccg[f])
   
   
 }
 
-f1 <- read_csv(f_ccg[1]) %>% mutate(file = f_ccg[1])
+m1 <- map(f_ccg, read_csv )
+map(m1, head) 
+
+f1 <- read_csv(f_ccg[4]) %>% mutate(file = f_ccg[4])
 names(f1)
 f1_l <- f1 %>%
   slice(2:nrow(.)) %>%
@@ -145,3 +202,56 @@ ncol_ccg1 <- purrr::map(ncol_ccg, ncol)
 ccg[1] %>%
   data.frame() %>%
   tidyr::gather(metrics, values, Numerator:Sigband)
+
+
+#### Mapping the data
+
+We can map these data -  a quick way is to read the GeoJSON files from the [ONS Geography Portal](http://geoportal.statistics.gov.uk/datasets/1bc1e6a77cdd4b3a9a0458b64af1ade4_2) and plot with `tmap` package.
+
+```{r create a map, cache=TRUE}
+if(!require("ggmap"))install.packages("ggmap")
+library(ggmap)
+if(!require("tmap"))install.packages("tmap")
+library(tmap)
+library(rgeos)
+if(!require("rgdal"))install.packages("rgdal")
+library(rgdal)
+if(!require("geojsonio"))install.packages("geojsonio")
+library(geojsonio)
+library(gganimate)
+if(!require("viridis"))install.packages("viridis")
+library(viridis)
+
+## get super generalised CCG shape file from the ONS geography portal (use 2015 boundaries which have 209 CCGs)
+
+ccg_map <- geojson_read("http://geoportal.statistics.gov.uk/datasets/67993b98f52743899751f188c960f7df_3.geojson", what = "sp")
+
+
+
+ccg <- fortify(ccg_map, region = "ccg15cd")
+
+ccg_map1<- ccg %>%
+  left_join(map1, by = c("id" = "CCG code"))
+
+palette <- rev(plasma(15))
+credits <- "Contains ordnance survey data (c) \nCrown copyright and database right 2016"
+
+
+g <- ccg_map1 %>%
+  filter(stringr::str_detect(Period , "Q" )) %>%
+  ggplot() +
+  geom_polygon(
+    aes(long, lat, 
+        group = group,
+        fill = Rate)) +
+  coord_map() + theme_minimal() + 
+  scale_fill_viridis(name = "Rate", option = "plasma") +
+  theme(axis.text = element_blank(), axis.title = element_blank()) +
+  facet_wrap(~Period)
+
+
+g
+
+```
+
+
